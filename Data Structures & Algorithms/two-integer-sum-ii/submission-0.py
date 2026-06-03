3b6fcd3a0b58346639750afde99ef5c0742fe909
angle <lengocangelina@gmail.com>
2026-06-02 21:43:13 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum < target:
+                l += 1 # shift the the right 1
+            elif sum > target:
+                r -= 1 # shift to the left 1
+            else: 
+                return [l + 1, r + 1] # since the question specifies for 1-indexed
