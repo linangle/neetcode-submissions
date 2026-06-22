84abf5c0fd7ae0d482d3e4b75978869fa08ff64b
angle <lengocangelina@gmail.com>
2026-06-22 00:00:55 -0400

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+    # greedy optimal
+    # idea : when we merge, take max --> values can only increase
+        # if any triplet has greater value than target, cannot be used
+    
+        # initialize 3 boolean flags, each checking for each target triplet value
+        x = y = z = False
+
+        for t in triplets:
+            # |= is or= (stays true if reaches true once)
+            x |= (t[0] == target[0] and t[1] <= target[1] and t[2] <= target[2])
+            y |= (t[1] == target[1] and t[0] <= target[0] and t[2] <= target[2])
+            z |= (t[2] == target[2] and t[0] <= target[0] and t[1] <= target[1])
+
+        if x and y and z:
+            return True
+        return False
